288f979b01b6d24ae383a7d1cdc4b4280da87d3e
koolrags <Juju@Raaghavs-MacBook-Pro-3.local>
2015-03-04 20:46:11 -0500

    Added string to right function

--- src/com/frame/app/View/Text_post.java
+++ src/com/frame/app/View/Text_post.java
@@ -10,6 +10,9 @@ import android.widget.EditText;
 
 import com.frame.app.R;
 
+import java.io.ByteArrayOutputStream;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Text_post extends ActionBarActivity {
 
@@ -45,15 +48,49 @@ public class Text_post extends ActionBarActivity {
     public void changeToMain(View view){
         Intent intent;
         intent = new Intent(this,MainPage.class);
-        EditText editText =(EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
         this.startActivity(intent);
 
     }
+
     public void changeToMainAfterPosting(View view){
         Intent intent;
         intent = new Intent(this,MainPage.class);
+        EditText editText =(EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        System.out.print(message + "\n");
+        /*JSONObject json = TextToJson(message,null,null,null,null,null);
+        StringRepresentation stringRep = new StringRepresentation(
+                obj.toString());
+        stringRep.setMediaType(MediaType.APPLICATION_JSON);
+
+        try {
+            res.post(stringRep).write(System.out);
+        } catch (ResourceException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }*/
+
         this.startActivity(intent);
 
+    }
+
+
+
+    public static JSONObject TextToJson(String text, Double lat, Double lon, String user, String date, String[] tags)
+    {
+        JSONObject jo = new JSONObject();
+
+        try {
+            jo.put("Text", text);
+            jo.put("Lat",lat);
+            jo.put("Lon",lon);
+            jo.put("User", user);
+            jo.put("Date", date);
+            jo.put("Tags", tags);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return jo;
     }
 }
